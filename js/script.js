750a122c84d4a11cd3391670b166735943da51a6
lmorillo <laura.mmails@gmail.com>
2023-11-07 16:08:44 +0100

    ajout de la sélection d'adresses

--- js/script.js
+++ js/script.js
@@ -1,22 +1,53 @@
+//Initialisation
+
 let liste = document.getElementById("selection")
+let champSelection = document.getElementById("user-street")
 liste.style.display = "none"
 
-document.getElementById("user-street").addEventListener("input", function (event){
-   let saisie = event.target.value.split(" ").join("+")
+//Début de l'algorithme 
 
-   let adresses;
+champSelection.addEventListener("input", function (event){
+    liste.innerHTML = "";
+    let saisie = event.target.value.split(" ").join("+")
+
+    let listeAdresses;
     let urlApi = "https://api-adresse.data.gouv.fr/search/?q=";
      getAdresses();
     async function getAdresses () {
-    const res = await fetch(urlApi + saisie);
-    adresses = await res.json();
+    const res = await fetch(urlApi + saisie + "&limit=5");
+    listeAdresses = await res.json();
     
     liste.style.display = "flex"
-        
-    console.log(adresses.features.slice(1, 4))
-}
 
-})
+    let features = listeAdresses.features.slice(0,5)
+
+    features.forEach(element => {
+            let adresses = element.properties.label
+            let tableauAdresses = []
+            tableauAdresses.push(adresses)
+
+            tableauAdresses.forEach(adresse => {
+                let option = document.createElement("div")
+                option.setAttribute('id', 'selection-item')
+                liste.appendChild(option)
+                option.innerHTML = adresse
+            });
+
+        });
+
+};
+
+if (champSelection.value == '') {
+    liste.lenght = 0
+    liste.style.display = "none"
+}    
+
+});
+
+
+
+
+
 
 
 
